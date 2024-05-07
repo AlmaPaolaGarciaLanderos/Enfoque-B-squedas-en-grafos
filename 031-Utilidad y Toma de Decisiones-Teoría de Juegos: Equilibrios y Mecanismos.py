78069fe86d1b357d7dd58a29165a35cd187036f5
AlmaPaolaGarciaLanderos <162000855+AlmaPaolaGarciaLanderos@users.noreply.github.com>
2024-05-06 22:47:35 -0600

    Practicas P2 IA

---
+++ 031-Utilidad y Toma de Decisiones-Teoría de Juegos: Equilibrios y Mecanismos.py
@@ -0,0 +1,20 @@
+#Alma Paola Garcia Landeros
+#21110038
+#6E1
+#Inteligencia Artificial
+#Centro de Enseñanza Tecnica Industrial           
+
+import numpy as np
+import nashpy as nash
+
+# Definir las matrices de pago del juego
+payoff_player1 = np.array([[3, 2], [0, 1]])
+payoff_player2 = np.array([[3, 0], [2, 1]])
+
+# Crear el juego
+game = nash.Game(payoff_player1, payoff_player2)
+
+# Encontrar el equilibrio de Nash
+equilibria = game.support_enumeration()
+for eq in equilibria:
+    print("Equilibrio de Nash:", eq)
